e35411e00dcfb7f0377cc345f713ca3260563480
bolishettianjali <bolishettianjali@gamil.com>
2021-12-05 15:49:34 +0530

    Added: welcome message

---
+++ src/main/java/com/cabInvoiceGenerator/InvoiceService.java
@@ -0,0 +1,7 @@
+package com.cabInvoiceGenerator;
+
+public class InvoiceService {
+    public static void main(String[] args){
+        System.out.println("Welcome to Cab invoice generator");
+    }
+}
